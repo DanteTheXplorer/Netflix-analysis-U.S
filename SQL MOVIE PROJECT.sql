4af029df9050a7dbfafa61f8a07d89e86a687db0
DanteTheXplorer <onyenwechukwuebukaojg@gmail.com>
2024-10-04 13:03:22 +0100

    Add files via upload

---
+++ SQL MOVIE PROJECT.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 20.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{5FA08A96-F3BA-4788-B4B9-565F7C6AC8CD}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {47DC2C54-BE98-4443-A14B-17A618B4B2B4}
+	EndGlobalSection
+EndGlobal
